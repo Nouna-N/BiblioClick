f1f1349fe8212c5ca13980eeaf7aa9bb41487b6e
Nouna-N <chahminouhaila4@gmail.com>
2025-04-24 12:59:32 +0100

    version-finale

--- backend/server.js
+++ backend/server.js
@@ -46,5 +46,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Serveur démarré sur le port ${PORT}`));
+app.listen(5000, '0.0.0.0', () => {
+  console.log("Serveur backend lancé sur le port 5000");
+});
